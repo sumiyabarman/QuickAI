908606a8b8b0aa1ecb55e826169082a39961489a
Sumiya Barman <sumiyabarman573@gmail.com>
2026-02-04 09:14:54 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import BlogTitles from './pages/BlogTitles'
+import Community from './pages/Community'
+import GenerateImages from './pages/GenerateImages'
+import RemoveBackground from'./pages/RemoveBackground'
+import RemoveObject from './pages/RemoveObject'
+import ReviewResume from './pages/ReviewResume'
+import WriteArticle from './pages/WriteArticle'
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/ai' element={<Layout/>}>
+        <Route index element={<Dashboard/>}/>
+        <Route path='write-article' element={<WriteArticle/>}/>
+        <Route path='blog-title' element={<BlogTitles/>}/>
+        <Route path='generate-image' element={<GenerateImages/>} />
+        <Route path='remove-background' element={<RemoveBackground/>} />
+        <Route path='remove-object' element={<RemoveObject/>} />
+        <Route path='review-resume' element={<ReviewResume/>} />
+        <Route path='community' element={<Community/>} />
+        </Route>
+      </Routes>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../componenets/Navbar'
+import Hero from '../componenets/Hero'
+import AiTools from '../componenets/AiTools'
+
+const Home = () => {
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <AiTools/>
+    </>
+  )
+}
+
+export default Home
